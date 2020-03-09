d11ab6bf2326b8d19b84ce8bf9098ba7144cabe7
Min Kim <min.c.kim@gmail.com>
2020-03-09 16:38:12 -0500

    Added contact get request

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+const Contact = require("../models/Contacts");
 
 // @route    GET api/contacts
 // @desc     Get user contacts
 // @access   Private
-router.get("/", (req, res) => {
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
   res.send("Get contacts");
 });
 // @route    POST api/contacts
