5c12634dfc1859824772d0ce5d2e45ae0d9b42d7
Min Kim <min.c.kim@gmail.com>
2020-03-10 01:57:43 -0500

    Created contact context api

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,22 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import ContactState from "./context/contact/ContactState";
 const App = () => {
   return (
-    <BrowserRouter>
-      <Fragment className='App'>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </BrowserRouter>
+    <ContactState>
+      <BrowserRouter>
+        <Fragment className='App'>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </BrowserRouter>
+    </ContactState>
   );
 };
 
