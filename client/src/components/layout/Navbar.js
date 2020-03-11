922eb4aed743e2fb14d4e1c94501b03486f55ee6
Min Kim <min.c.kim@gmail.com>
2020-03-11 15:24:05 -0500

    Changed logout link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,25 +1,34 @@
-import React, { useContext, Fragment } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
 import ContactContext from "../../context/contact/contactContext";
+
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
   const contactContext = useContext(ContactContext);
-  const { isAuthenticated, logout, user } = authContext;
+
+  const { isAuthenticated, logout, user, loadUser } = authContext;
   const { clearContacts } = contactContext;
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
+
   const onLogout = () => {
     logout();
     clearContacts();
   };
+
   const authLinks = (
     <Fragment>
       <li>Hello {user && user.name}</li>
       <li>
-        <Link onClick={onLogout}>
-          <i className='fa fa-sign-out-alt mr1'></i>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{" "}
           <span className='hide-sm'>Logout</span>
-        </Link>
+        </a>
       </li>
     </Fragment>
   );
@@ -34,10 +43,13 @@ const Navbar = ({ title, icon }) => {
       </li>
     </Fragment>
   );
+
   return (
     <div className='navbar bg-primary'>
       <h1>
-        <i className={icon} /> {title}
+        <Link to='/'>
+          <i className={icon} /> {title}
+        </Link>
       </h1>
       <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
@@ -46,11 +58,12 @@ const Navbar = ({ title, icon }) => {
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icone: PropTypes.string
+  icon: PropTypes.string
 };
 
 Navbar.defaultProps = {
   title: "Contact Keeper",
-  icon: "fa fa-id-card-alt"
+  icon: "fas fa-id-card-alt"
 };
+
 export default Navbar;
